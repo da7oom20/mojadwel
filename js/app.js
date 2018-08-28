0e2320aabcb642678ecced55c4f9abb8fa2aa9a8
Zayed Alabbad <zayed343@gmail.com>
2018-08-28 19:09:58 +0300

    export timetable as an image feature

--- js/app.js
+++ js/app.js
@@ -717,3 +717,25 @@ $("#added-sections-table").on("click", ".remove-button", function () {
         icon: "success"
     });
 });
+
+// Export timetable as image
+$("#export-as-image").click(function() {
+    function saveAs(uri, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
+            //Firefox requires the link to be in the body
+            document.body.appendChild(link);
+            //simulate click
+            link.click();
+            //remove the link when done
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
+    }
+    html2canvas(document.querySelector('#timetable')).then(function(canvas) {
+        saveAs(canvas.toDataURL(), 'جدول.png');
+    });
+});
